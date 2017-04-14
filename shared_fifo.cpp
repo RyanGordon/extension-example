83b276659286ab43aeb23f5afa242c4c0fce71ea
Ryan Gordon <ryan@quizlet.com>
2017-04-14 12:14:30 -0700

    Fixes

--- shared_fifo.cpp
+++ shared_fifo.cpp
@@ -16,56 +16,84 @@
 
 #include "hphp/runtime/ext/extension.h"
 #include "hphp/util/lock.h"
+#include "hphp/util/mutex.h"
 #include <unordered_map>
+#include <queue>
+#include <string>
 
 namespace HPHP {
 
-static Mutex shared_fifo_mutex;
-static std::unordered_map<std::string, SharedFifo> shared_fifos;
+
+class SharedFifo {
+  ReadWriteMutex fifo_queue_mutex;
+  const String& name;
+  std::queue<std::string> fifo_queue;
+public:
+  SharedFifo(const String& _name) : name(_name) { }
+
+  void push(const String& value) {
+    WriteLock write_lock(fifo_queue_mutex);
+    fifo_queue.push(value.toCppString());
+  }
+
+  Variant pop() {
+    WriteLock write_lock(fifo_queue_mutex);
+
+    if (fifo_queue.size() == 0) {
+      return Variant(Variant::NullInit{});
+    }
+
+    const std::string value = fifo_queue.front();
+  fifo_queue.pop();
+  return Variant(String(value));
+   }
+};
+
+
+static ReadWriteMutex shared_fifo_mutex;
+static std::unordered_map<std::string, SharedFifo*> shared_fifos;
 
 static bool HHVM_FUNCTION(shfifo_init, const String& name) {
+  const std::string cpp_name = name.toCppString();
+
   {
-    ReadLock(shared_fifo_mutex);
-    std::unordered_map<std::string, SharedFifo>::const_iterator shared_fifo = shared_fifos.find(name);
+    ReadLock read_lock(shared_fifo_mutex);
+    std::unordered_map<std::string, SharedFifo*>::const_iterator shared_fifo = shared_fifos.find(cpp_name);
     if (shared_fifo != shared_fifos.end()) return false;
   }
 
-  SharedFifo shared_fifo = new SharedFifo(name);
-  
   {
-    WriteLock(shared_fifo_mutex);
+  WriteLock write_lock(shared_fifo_mutex);
 
     // Have to gate against a race condition by checking if it was just created here!
-    std::unordered_map<std::string, SharedFifo>::const_iterator shared_fifo = shared_fifos.find(name);
+    std::unordered_map<std::string, SharedFifo *>::const_iterator shared_fifo = shared_fifos.find(cpp_name);
     if (shared_fifo != shared_fifos.end()) return false;
 
-    shared_fifos.insert(std::pair<std::string, SharedFifo>{name, shared_fifo});
+    shared_fifos[cpp_name] = new SharedFifo(name);
   }
 
   return true;
 }
 
 static bool HHVM_FUNCTION(shfifo_push, const String& pool_name, const String& value) {
-  {
-    ReadLock(shared_fifo_mutex);
-    std::unordered_map<std::string, SharedFifo>::const_iterator shared_fifo = shared_fifos.find(name);
-    if (shared_fifo == shared_fifos.end()) return false;
-  }
+  ReadLock read_lock(shared_fifo_mutex);
+  std::unordered_map<std::string, SharedFifo *>::const_iterator shared_fifo = shared_fifos.find(pool_name.toCppString());
+  if (shared_fifo == shared_fifos.end()) return false;
 
-  return shared_fifo.push(value);
+  shared_fifo->second->push(value);
+
+  return true;
 }
 
 static Variant HHVM_FUNCTION(shfifo_pop, const String& pool_name, const String& value) {
-  {
-    ReadLock(shared_fifo_mutex);
-    std::unordered_map<std::string, SharedFifo>::const_iterator shared_fifo = shared_fifos.find(name);
-    if (shared_fifo == shared_fifos.end()) return false;
-  }
+  ReadLock read_lock(shared_fifo_mutex);
+  std::unordered_map<std::string, SharedFifo *>::const_iterator shared_fifo = shared_fifos.find(pool_name.toCppString());
+  if (shared_fifo == shared_fifos.end()) return Variant(false);
 
-  return shared_fifo.pop();
+  return shared_fifo->second->pop();
 }
 
-static class SharedFifoExtension : public Extension {
+class SharedFifoExtension : public Extension {
  public:
   SharedFifoExtension() : Extension("shared_fifo") {}
   virtual void moduleInit() {
@@ -74,35 +102,8 @@ static class SharedFifoExtension : public Extension {
     HHVM_FE(shfifo_pop);
     loadSystemlib();
   }
-} s_shared_fifo;
+} s_shared_fifo_extension;
 
-static class SharedFifo {
-  static Mutex fifo_queue_mutex;
-  const String& name;
-  const std::queue<std::string> fifo_queue;
-public:
-  SharedFifo(const String& _name) {
-    name = _name;
-  }
-
-  bool push(const String& value) {
-    WriteLock(fifo_queue_mutex);
-    fifo_queue.push(value);
-
-    return true;
-  }
-
-  Variant pop() {
-    WriteLock(fifo_queue_mutex);
-
-    if (fifo_queue_mutex.size() === 0) {
-      return Variant(Variant::NullInit{});
-    }
-
-    return fifo_queue.pop();
-  }
-}
-
-HHVM_GET_MODULE(shared_fifo)
+HHVM_GET_MODULE(shared_fifo);
 
 } // namespace HPHP
